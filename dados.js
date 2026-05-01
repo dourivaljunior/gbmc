bb10d18335bcf3b3e8c02270dc0a70e80527c533
Prof. Dourival Júnior <100372396+dourivaljunior@users.noreply.github.com>
2026-05-01 16:02:33 -0300

    Add ACERVO_MUSICAL object with music data

---
+++ dados.js
@@ -0,0 +1,19 @@
+const ACERVO_MUSICAL = {
+    "Rock": {
+        "Raul Seixas": [
+            { 
+                titulo: "Gita", 
+                letra_id: "ID_DO_ARQUIVO_PDF", 
+                musica_id: "ID_DO_ARQUIVO_MP3", 
+                bt_id: "ID_DO_ARQUIVO_BT" 
+            },
+            { titulo: "Metamorfose Ambulante", letra_id: "ID_2", musica_id: "ID_3", bt_id: "ID_4" }
+        ],
+        "Engenheiros do Hawaii": [],
+        "Legião Urbana": []
+    },
+    "Forró": {},
+    "Sertanejo": {},
+    "MPB": {},
+    "Samba": {}
+};
